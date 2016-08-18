4607ec56fba28d5a41a8bd99016aa267a3263cc1
Nils Wloka <nils.wloka@codecentric.de>
2016-08-18 16:15:53 +0200

    Removed configuration parameters from staging config

--- project_name/settings.py
+++ project_name/settings.py
@@ -114,17 +114,7 @@ class Development(Common):
     ALLOWED_HOSTS = []
 
 class Staging(Common):
-    SESSION_COOKIE_SECURE = values.BooleanValue(True)
-    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
-    SECURE_REDIRECT_EXEMPT = values.ListValue([])
-    SECURE_SSL_HOST = values.Value(None)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
-    SECURE_PROXY_SSL_HEADER = values.TupleValue(
-        ('HTTP_X_FORWARDED_PROTO', 'https')
-    )
+    pass
 
 class Production(Staging):
     pass
