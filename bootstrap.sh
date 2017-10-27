957e87c6a592797059be9d5fcd3c939ebdfcf290
Nils Wloka <nils.wloka@codecentric.de>
2017-10-27 12:56:01 +0200

    Change repo location to codecentric organization

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,12 +25,12 @@ if [ $? -ne 0 ]; then
   echo >&2 "You can find instruction for doing so at https://help.github.com/articles/generating-an-ssh-key/."
   exit 1;
 fi
-git ls-remote git@github.com:codecentric-labs-zero/$PROJECT_NAME.git >/dev/null 2>&1
+git ls-remote git@github.com:codecentric/$PROJECT_NAME.git >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo "Warning! There is an existing GitHub repository at git@github.com:codecentric-labs-zero/$PROJECT_NAME.git."
+  echo "Warning! There is an existing GitHub repository at git@github.com:codecentric/$PROJECT_NAME.git."
   echo "This can happen if you are retrying to bootstrap this project and this script has already created it before."
   echo "If you are running this script for the first time, you should probably abort bootstrapping with CTRL-C."
-  echo "Check the codecentric-labs-zero organisation on GitHub and use a unique project name next time."
+  echo "Check the codecentric organisation on GitHub and use a unique project name next time."
   read -p "If you still want to continue, press [ENTER]."
 fi
 if [ -z "$CIRCLE_TOKEN" ]; then
@@ -65,10 +65,10 @@ echo "=== Fixing file permissions ==="
 chmod u+x manage.py >/dev/null 2>&1
 chmod u+x merge-base-ff.sh >/dev/null 2>&1
 echo "=== Setting up GitHub and CircleCI ==="
-git ls-remote git@github.com:codecentric-labs-zero/$PROJECT_NAME.git >/dev/null 2>&1
+git ls-remote git@github.com:codecentric/$PROJECT_NAME.git >/dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "Creating repository"
-  curl -sS -X POST -d '{"name": "'"$PROJECT_NAME"'", "private": false, "team_id": "2073794"}' -H "Authorization: token $GITHUB_TOKEN" -i https://api.github.com/orgs/codecentric-labs-zero/repos >/dev/null
+  curl -sS -X POST -d '{"name": "'"$PROJECT_NAME"'", "private": false, "team_id": "2049986"}' -H "Authorization: token $GITHUB_TOKEN" -i https://api.github.com/orgs/codecentric/repos >/dev/null
   if [ $? -ne 0 ]; then
     echo >&2 "Creating repository failed. Please have a look at the console output and fix all problem."
     echo >&2 "You can re-run bootstrap.sh at any time to retry bootstrapping this project."
@@ -79,7 +79,7 @@ else
   echo "Repository already exists, skipping creation."
 fi
 echo "Adding repository to CircleCI"
-curl -sS -X POST "https://circleci.com/api/v1.1/project/github/codecentric-labs-zero/$PROJECT_NAME/follow?circle-token=$CIRCLE_TOKEN" >/dev/null
+curl -sS -X POST "https://circleci.com/api/v1.1/project/github/codecentric/$PROJECT_NAME/follow?circle-token=$CIRCLE_TOKEN" >/dev/null
 echo "Creating new SSH key"
 ssh-keygen -t rsa -b 4096 -C "cc-labs-zero@codecentric.de" -N "" -f $TMPDIR$PROJECT_NAME-key >/dev/null
 PRIVATE_KEY="`cat $TMPDIR$PROJECT_NAME-key`"
@@ -87,9 +87,9 @@ PUBLIC_KEY="`cat $TMPDIR$PROJECT_NAME-key.pub`"
 rm $TMPDIR$PROJECT_NAME-key >/dev/null
 rm $TMPDIR$PROJECT_NAME-key.pub >/dev/null
 echo "Adding SSH key to CircleCI"
-curl -sS -X POST --header "Content-Type: application/json" -d '{"hostname":"github.com", "private_key":"'"$PRIVATE_KEY"'"}' "https://circleci.com/api/v1.1/project/github/codecentric-labs-zero/$PROJECT_NAME/ssh-key?circle-token=$CIRCLE_TOKEN" >/dev/null
+curl -sS -X POST --header "Content-Type: application/json" -d '{"hostname":"github.com", "private_key":"'"$PRIVATE_KEY"'"}' "https://circleci.com/api/v1.1/project/github/codecentric/$PROJECT_NAME/ssh-key?circle-token=$CIRCLE_TOKEN" >/dev/null
 echo "Adding SSH key to GitHub"
-curl -sS -X POST -d '{"key": "'"$PUBLIC_KEY"'", "title": "CircleCI write access", "read_only": false }' -H "Authorization: token $GITHUB_TOKEN" -i https://api.github.com/repos/codecentric-labs-zero/$PROJECT_NAME/keys >/dev/null
+curl -sS -X POST -d '{"key": "'"$PUBLIC_KEY"'", "title": "CircleCI write access", "read_only": false }' -H "Authorization: token $GITHUB_TOKEN" -i https://api.github.com/repos/codecentric/$PROJECT_NAME/keys >/dev/null
 echo "Adding Heroku API key to CircleCI"
 curl -sS --data "apikey=`heroku auth:token`" "https://circleci.com/api/v1.1/user/heroku-key?circle-token=$CIRCLE_TOKEN" >/dev/null
 echo "================================================================================================================================"
@@ -97,7 +97,7 @@ echo "You need to manually associate a Heroku SSH key with your CircleCI account
 echo "Please visit https://circleci.com/gh/codecentric-labs-zero/$PROJECT_NAME/edit#heroku and follow the instructions given in Step 2."
 read -p "Press [Enter] when you have completed this step."
 echo "Adding origin remote to local repository"
-git remote add origin git@github.com:codecentric-labs-zero/$PROJECT_NAME.git >/dev/null
+git remote add origin git@github.com:codecentric/$PROJECT_NAME.git >/dev/null
 echo "=== Setting up Heroku ==="
 heroku create $APP_NAME --org codecentric-labs-zero --remote staging --region eu
 if [ $? -ne 0 ]; then
@@ -132,7 +132,7 @@ heroku pipelines:add $PIPELINE_NAME --app $APP_NAME --stage staging
 echo "=== Done setting up Heroku ==="
 echo "Writing new README.md"
 echo "# $PROJECT_NAME" > README.md
-echo "[![CircleCI](https://circleci.com/gh/codecentric-labs-zero/$PROJECT_NAME.svg?style=svg)](https://circleci.com/gh/codecentric-labs-zero/$PROJECT_NAME)" >> README.md
+echo "[![CircleCI](https://circleci.com/gh/codecentric/$PROJECT_NAME.svg?style=svg)](https://circleci.com/gh/codecentric/$PROJECT_NAME)" >> README.md
 echo "## Links" >> README.md
 echo "### Staging environment" >> README.md
 echo "* [Web application](https://$APP_NAME.herokuapp.com/web)" >> README.md
